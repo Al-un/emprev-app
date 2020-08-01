3248b251ee754cde5babc156aa98e45689a4a633
Al-un <alun.sng@gmail.com>
2020-08-01 16:11:03 +0900

    feat: add global SCSS import

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
+  css: {
+    loaderOptions: {
+      // key changed from `sass` to `scss`
+      // https://github.com/vuejs/vue-cli/issues/4526#issuecomment-537028918
+      scss: {
+        prependData: `@use "~@/styles/_includes.scss" as *;`
+      }
+    }
+  },
+
+  transpileDependencies: ['vuetify'],
 
   pluginOptions: {
     i18n: {
-      locale: "en",
-      fallbackLocale: "en",
-      localeDir: "i18n",
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'i18n',
       enableInSFC: false
     }
   }
-};
+}
