f14dd2a54e96df26447d64156e689f501ace0ee7
Al-un <alun.sng@gmail.com>
2020-08-01 16:11:43 +0900

    feat: add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,53 @@
+import { configure } from "@storybook/vue";
+import { withKnobs, boolean, select } from "@storybook/addon-knobs";
+
+import i18n from "../src/plugins/i18n";
+import vuetify from "../src/plugins/vuetify";
+
+const i18nDecorator = () => ({
+  // https://medium.com/studist-dev/internationalization-in-storybook-4be77773494c
+  i18n,
+  template: `<story />`,
+  props: {
+    storybookLocale: {
+      type: String,
+      default: select("I18n locale", ["en", "fr"], "en", "VuePlugins"),
+    },
+  },
+  watch: {
+    storybookLocale: {
+      handler() {
+        this.$i18n.locale = this.storybookLocale;
+      },
+      immediate: true,
+    },
+  },
+  created() {
+    // console.log(`=== i18n messages ===`, this.$i18n.messages)
+  },
+});
+
+const vuetifyDecorator = () => ({
+  // https://github.com/nidkil/vuetify-with-storybook/blob/master/config/storybook/config.js
+  vuetify,
+  template: "<v-app><story/></v-app>",
+  props: {
+    vuetifyDark: {
+      type: Boolean,
+      default: boolean("Vuetify Dark theme", true, "VuePlugins"),
+    },
+  },
+  watch: {
+    vuetifyDark: {
+      handler() {
+        this.$vuetify.theme.dark = this.vuetifyDark;
+      },
+      immediate: true,
+    },
+  },
+});
+
+addDecorator(withKnobs);
+addDecorator(vuetifyDecorator);
+addDecorator(i18nDecorator);
+configure(require.context("../src/components/", true, /\.stories\.ts$/), module);
