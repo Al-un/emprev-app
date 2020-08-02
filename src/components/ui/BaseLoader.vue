b9c0e31170ae6d30b7d5d7a56f38990babfd386f
Al-un <alun.sng@gmail.com>
2020-08-02 09:51:30 +0900

    feat: add loader global component

---
+++ src/components/ui/BaseLoader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Loading...
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'BaseLoader',
+
+  setup() {
+    return {}
+  },
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import i18n from "./plugins/i18n";
-import "./plugins/composition";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import i18n from './plugins/i18n'
+import './plugins/composition'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+import BaseLoader from '@/components/ui/BaseLoader.vue'
+
+Vue.component('base-loader', BaseLoader)
 
 new Vue({
   router,
   store,
   vuetify,
   i18n,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
