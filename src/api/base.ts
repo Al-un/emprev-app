9a3cd1fb69e55b2a81078a82b9b787874a3d728c
Al-un <alun.sng@gmail.com>
2020-08-03 18:56:05 +0900

    feat: define API base url based on environment variable

--- src/api/base.ts
+++ src/api/base.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import { ApiReqPayload } from '@/models'
 
-const baseURL = 'http://localhost:8000'
+const baseURL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8000'
 
 class ApiBase {
   httpClient = axios.create({
